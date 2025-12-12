7b3d9965ad04b5dac8599724a9cf63a8d8021652
Athira-rajalekshmi <athirardas56@gmail.com>
2025-12-12 16:24:53 +0100

    Removed failing code

--- tests/api_Test.spec.js
+++ tests/api_Test.spec.js
@@ -2,34 +2,16 @@ import { test, expect, request } from '@playwright/test'
 
 test.describe('API Functionalities Demo', () => {
 
-    test.only('GET FUNCTION', async ({ request }) => {
+    test('GET FUNCTION', async ({ request }) => {
 
         const res = await request.get('https://disease.sh/v3/covid-19/countries/USA?yesterday=true')
+        
         expect(res.status()).toBe(200)
+        expect(response.ok()).toBeTruthy();
 
         const text = await res.text()
         expect(text).toContain("USA")
-
+        
         console.log(await res.json())
     })
-
-    test('POST FUNCTION', async ({ request }) => {
-
-        const response = await request.post("http://127.0.0.1:11434/api/generate", {
-            headers: {
-                "Content-Type": "application/json"
-            },
-            data: {
-                model: "llama2",
-                prompt: "Why is the sky blue?",
-                options: {
-                    temperature: 0.7
-                }
-            }
-        });
-
-        expect(response.ok()).toBeTruthy();
-        console.log(await response.json());
-    })
-
 })
